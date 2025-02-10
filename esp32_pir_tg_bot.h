c3843c64c64041e7e2ac362970bd7d5e006dca4d
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2025-02-10 14:45:22 +0300

    Update esp32_pir_tg_bot.h

--- esp32_pir_tg_bot.h
+++ esp32_pir_tg_bot.h
@@ -2,6 +2,7 @@
 #pragma GCC diagnostic ignored "-fpermissive"
 #define _WANT_USE_LONG_TIME_T
 #define _USE_LONG_TIME_T
+//#define FB_NO_FILE
 #include <MAIN.h>
 #include <FastBot2.h>
 #include <GyverIO.h>
@@ -13,10 +14,11 @@
 #include "time.h"
 #ifndef CONFIG_BT_BLE_50_FEATURES_SUPPORTED
 #warning "Not compatible hardware"
+#define NO_BLE
 #endif
 #include <BLEDevice.h>
 #include <BLEAdvertisedDevice.h>
-#define MAIN_TASK_STACK_SIZE (16 * 1024)
+
 #if CORE_DEBUG_LEVEL
 #define DEBUG_ENABLE
 #endif
@@ -29,6 +31,7 @@
 #define BOT_TOKEN ""
 #define USER_ID "" 
 #define CHAT_ID USER_ID
+#define SSID_HIDDEN 0
 #else
 #define DEBUG(x)
 #define DEBUGLN(x) 
@@ -38,38 +41,15 @@
 #define BOT_TOKEN ""
 #define USER_ID "" 
 #define CHAT_ID "" 
+#define SSID_HIDDEN 1
 #endif // DEBUG_ENABLE
 #define ERR_CHECK(x) ESP_ERROR_CHECK_WITHOUT_ABORT(x)
-#define ESP32C3_LUATOS
+//#define ESP32C3_LUATOS
+#define NO_BLE
 #define AP_SSID ""
 #define DEFAULT_SSID ""
 #define DEFAULT_PASS ""
-#ifdef CONFIG_IDF_TARGET_ESP32
-#define PIN_LINE 15
-#define PIN_BUTTON 0
-#define PIN_LED 2
-#define PIN_RELAY 4
-#elif defined CONFIG_IDF_TARGET_ESP32C3
-#define PIN_LINE 4
-#define PIN_BUTTON 9
-#define PIN_RELAY 8
-#if defined ESP32C3_LUATOS
-#define PIN_LED_D5 13
-#define PIN_LED 12
-#endif
-#else
-#define PIN_LED 8
-#endif
-#if defined ESP32C3_LUATOS
-#define LED_ON	HIGH
-#define LED_OFF LOW
-#elif defined CONFIG_IDF_TARGET_ESP32C3
-#define LED_ON	LOW
-#define LED_OFF HIGH
-#endif
-
 #define WIFI_CHANNEL 13
-#define SSID_HIDDEN 1
 #define AP_PASS ""
 #define CHANGE_AUTH "/config"
 #define BLE_SET "/ble_set"
@@ -78,74 +58,114 @@
 #define SSID_PATH "/ssid.txt"
 #define PASS_PATH "/pass.txt"
 #define ALARM_PATH "/alarm.txt"
+#ifdef CONFIG_IDF_TARGET_ESP32
+#define PIN_LINE 15
+#define PIN_PULLUP 14
+#define PIN_BUTTON 0
+#define PIN_LED 2
+#define PIN_RELAY 4
+#define LED_ON	HIGH
+#define LED_OFF LOW
+#elif defined CONFIG_IDF_TARGET_ESP32C3
+#define PIN_LINE 4
+#define PIN_PULLUP 5
+#define PIN_BUTTON 9
+#define PIN_RELAY 8
+#define LED_ON	LOW
+#define LED_OFF HIGH
+#if defined ESP32C3_LUATOS
+#define PIN_LED_D5 12
+#define PIN_LED 13
+#else
+#define PIN_LED 8
+#endif
+#endif
+
+#define MAIN_TASK_STACK_SIZE (8 * 1024)
+#define SEND_TASK_STACK_SIZE (8 * 1024)
+#define QUEUE_ITEM_SIZE (sizeof(tgMessage_t))
+#define QUEUE_SIZE (32 * QUEUE_ITEM_SIZE)
 
 #define lineRead gio::read(PIN_LINE)
 #define dWrite(pin, val) gio::write(pin, val)
 #define dRead(pin) gio::read(pin)
 #define uS esp_timer_get_time()
+#define Delay(x) vTaskDelay(pdMS_TO_TICKS(x));
 #define TIMER_RECONNECT	60 * 60 * 1000000ul
 #define TIMER_SABOTAGE	2500000
-#define TIMER_CHECK		1000000
+#define TIMER_CHECK	1000000
 #define TIMER_RESEND	60 * 1000000ul
 typedef uint32_t _time_t;
 using namespace fb;
 
 typedef enum : uint8_t {
-	LINE_OK,
+	ok = 0,
 	ALARM,
 	LINE_HIGH,
 	LINE_LOW,
+	RESEND_MSG,
 	CHECK_MSG,
 	WIFI_DISCONNECT,
 	WIFI_INIT,
 	WIFI_RECON,
-	RESEND_MSG,
 	RESTART
 } stat_t;
 
-static StackType_t xMainStack[MAIN_TASK_STACK_SIZE];
-static StaticTask_t xMainTaskBuffer;
-TaskHandle_t mainTaskHandle;
+typedef struct __attribute__((packed)) {
+	uint32_t delta;
+	stat_t status;
+} tgMessage_t;
 
-static volatile stat_t status;
-stat_t prev_status = LINE_OK, interrupt_flag = CHECK_MSG;
-network_event_handle_t event_id = 0;
+StackType_t xMainStack[MAIN_TASK_STACK_SIZE], xSendStack[SEND_TASK_STACK_SIZE];
+StaticTask_t xMainTaskBuffer, xSendTaskBuffer;
+TaskHandle_t mainTaskHandle,sendTaskHandle;
+QueueHandle_t QueueStatHandle;
+StaticQueue_t pxStaticQueue;
+uint8_t QueueStatStorage[QUEUE_SIZE];
+
+//static volatile stat_t prev_alarm = ok;
+static stat_t Flag = ok;
+bool alarm_state = 0;
 volatile uint64_t last_interrupt = 0;
-volatile uint32_t alarm_delta = 0, interrupt_delta = 0;
+volatile uint32_t interrupt_delta = 0;
 _time_t timestamp_unix;
 uint64_t timestamp_sync;
-size_t timestamps_count;
-_time_t* timestamp_arr = nullptr;
+#ifndef NO_BLE
 byte* ble_data = nullptr;
 byte ble_data_size = 0;
-gptimer_handle_t tmrSab = nullptr, tmrWifi = nullptr;
-String* ssid = nullptr, * pass = nullptr;
-
+#endif
+network_event_handle_t event_id = 0;
+gptimer_handle_t tmr_sab = nullptr;
+String ssid, pass;
 AsyncWebServer server(80);
 FastBot2 bot;
 
+void mainTask(void*);
+void sendTask(void*);
+void setup(void*);
 static void IRAM_ATTR ISR();
-bool IRAM_ATTR sabotage_check(gptimer_handle_t tmr, const gptimer_alarm_event_data_t* edata, void* user_ctx);
-bool IRAM_ATTR wifi_check(gptimer_handle_t tmr, const gptimer_alarm_event_data_t* edata, void* user_ctx);
-static void timerAlarm(uint64_t value, stat_t flag, gptimer_handle_t& handle = tmrWifi);
+bool IRAM_ATTR sabotage_check(gptimer_handle_t, const gptimer_alarm_event_data_t*, void*);
 void read_credentials();
 void time_sync(byte wait_sec = 10);
-bool readFile(cch* path, String*& Content);
-bool writeFile(cch* path, String*& Content);
-bool readFile(cch* path, _time_t*& buf, size_t& size);
+bool readFile(cch* path, String& Content);
+bool writeFile(cch* path, const String& Content);
 bool appendFile(cch* path, _time_t value);
 bool deleteFile(cch* path);
+void onWiFiConnected(arduino_event_id_t event);
 static String get_info();
 void wifi_server_init();
 bool wifi_sta_init(byte wait_sec = 5);
 void onConfigRequest(AsyncWebServerRequest* request);
-void tg_send(cch* text, cch* chat = CHAT_ID);
 bool ble_advertising(const byte* ble_data, const byte ble_data_length, uint32_t time_ms = 500);
+stat_t send_alarm_time(Value const& chat_id);
+void tg_send(tgMessage_t&);
+void updateHandler(fb::Update& u);
+void handleMessage(fb::Update& u);
+void handleDocument(fb::Update& u);
+void otaBegin(fb::Update& u, bool fw);
 String create_hex_string(const byte* const& buf, const byte data_size);
 bool strtoB(const String& str, byte sub, byte*& buf, byte& data_len, const byte hexSizeMin = 6);
-bool send_alarm_time(Value const& chat_id);
-void updateHandler(fb::Update& u);
-void otaBegin(fb::Update& u, bool fw = true);
-void handleDocument(fb::Update& u);
-void handleMessage(fb::Update& u);
+void alarm_on() { alarm_state = true;/*enableInterrupt(PIN_LINE);*/ /*timer_restart(tmr_sab);timer_start(tmr_sab);*/ };
+void alarm_off() { alarm_state = false;/*disableInterrupt(PIN_LINE);*/  /*timer_stop(tmr_sab);*/ };
+void resumeTask(stat_t st) { Flag = st; xTaskAbortDelay(mainTaskHandle);/*vTaskResume(mainTaskHandle);*/ };
 bool verifyRollbackLater() { return true; };
