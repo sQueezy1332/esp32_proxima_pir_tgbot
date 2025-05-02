585524b1a33e41b1aa126930f61618dc1aa73a9f
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2025-05-03 02:16:00 +0300

    Update esp32_pir_tg_bot.h

--- esp32_pir_tg_bot.h
+++ esp32_pir_tg_bot.h
@@ -58,7 +58,22 @@
 #define SSID_PATH "/ssid.txt"
 #define PASS_PATH "/pass.txt"
 #define ALARM_PATH "/alarm.txt"
-#ifdef CONFIG_IDF_TARGET_ESP32
+#ifdef CONFIG_IDF_TARGET_ESP32C3
+#define PIN_LINE 4
+#define PIN_PULLUP 5
+#define PIN_BUTTON 9
+#define PIN_RELAY 8
+#if defined ESP32C3_LUATOS
+#define PIN_LED_D5 12
+#define PIN_LED 13
+#define LED_ON	HIGH
+#define LED_OFF LOW
+#else
+#define PIN_LED 8
+#define LED_ON	LOW
+#define LED_OFF HIGH
+#endif
+#else
 #define PIN_LINE 15
 #define PIN_PULLUP 14
 #define PIN_BUTTON 0
@@ -66,19 +81,6 @@
 #define PIN_RELAY 4
 #define LED_ON	HIGH
 #define LED_OFF LOW
-#elif defined CONFIG_IDF_TARGET_ESP32C3
-#define PIN_LINE 4
-#define PIN_PULLUP 5
-#define PIN_BUTTON 9
-#define PIN_RELAY 8
-#define LED_ON	LOW
-#define LED_OFF HIGH
-#if defined ESP32C3_LUATOS
-#define PIN_LED_D5 12
-#define PIN_LED 13
-#else
-#define PIN_LED 8
-#endif
 #endif
 
 #define MAIN_TASK_STACK_SIZE (8 * 1024)
@@ -90,10 +92,11 @@
 #define dWrite(pin, val) gio::write(pin, val)
 #define dRead(pin) gio::read(pin)
 #define uS esp_timer_get_time()
-#define Delay(x) vTaskDelay(pdMS_TO_TICKS(x));
+#define Delay(x) vTaskDelay(pdMS_TO_TICKS(x))
+#define DelayUs(x) ets_delay_us(x)
 #define TIMER_RECONNECT	60 * 60 * 1000000ul
 #define TIMER_SABOTAGE	2500000
-#define TIMER_CHECK	1000000
+#define TIMER_CHECK		1000000
 #define TIMER_RESEND	60 * 1000000ul
 typedef uint32_t _time_t;
 using namespace fb;
@@ -108,7 +111,7 @@ typedef enum : uint8_t {
 	WIFI_DISCONNECT,
 	WIFI_INIT,
 	WIFI_RECON,
-	RESTART
+	RESTART,
 } stat_t;
 
 typedef struct /*__attribute__((packed))*/ {
@@ -118,7 +121,7 @@ typedef struct /*__attribute__((packed))*/ {
 
 StackType_t xMainStack[MAIN_TASK_STACK_SIZE], xSendStack[SEND_TASK_STACK_SIZE];
 StaticTask_t xMainTaskBuffer, xSendTaskBuffer;
-TaskHandle_t mainTaskHandle,sendTaskHandle;
+TaskHandle_t loopTaskHandle,sendTaskHandle;
 QueueHandle_t QueueStatHandle;
 StaticQueue_t pxStaticQueue;
 uint8_t QueueStatStorage[QUEUE_SIZE];
@@ -135,7 +138,7 @@ byte* ble_data = nullptr;
 byte ble_data_size = 0;
 #endif
 network_event_handle_t event_id = 0;
-gptimer_handle_t tmr_sab = nullptr;
+gptimer_handle_t timer_sab = nullptr;
 String ssid, pass;
 AsyncWebServer server(80);
 FastBot2 bot;
@@ -152,6 +155,7 @@ bool writeFile(cch* path, const String& Content);
 bool appendFile(cch* path, _time_t value);
 bool deleteFile(cch* path);
 void onWiFiConnected(arduino_event_id_t event);
+String get_task_list();
 String get_info(bool ver = false);
 void wifi_server_init();
 bool wifi_sta_init(byte wait_sec = 5);
@@ -168,5 +172,42 @@ bool strtoB(const String& str, byte sub, byte*& buf, byte& data_len, const byte 
 void byte2hexstr(char* text, const byte* buf, const byte data_size);
 void alarm_on() { alarm_state = true;/*enableInterrupt(PIN_LINE);*/ /*timer_restart(tmr_sab);timer_start(tmr_sab);*/ };
 void alarm_off() { alarm_state = false;/*disableInterrupt(PIN_LINE);*/  /*timer_stop(tmr_sab);*/ };
-void resumeTask(status_t st) { Flag = st; xTaskAbortDelay(mainTaskHandle);/*vTaskResume(mainTaskHandle);*/ };
+void resumeTask(stat_t st = RESEND_MSG) { Flag = st; xTaskAbortDelay(loopTaskHandle);/*vTaskResume(mainTaskHandle);*/ };
 bool verifyRollbackLater() { return true; };
+
+void suicide_func() {
+	//extern StackType_t* shitstack;
+	uint32_t rnd = random(0x3FFAE000, 0x400B8000);
+	DEBUGF("address = 0x%X, value = %X\n", rnd, ESP_REG(rnd)); delay(5);
+	ESP_REG(rnd) = 0;
+	auto lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+	char* strtmp = (char*)malloc(56); memcpy(strtmp, lorem, 56); strtmp[55] = 0;
+	uint32_t addrstr = (uint32_t)strtmp;
+	DEBUGF("address strtmp = 0x%X\n", addrstr);
+	char* newptrstr = (char*)(addrstr);
+	for (size_t i = 0; i < 55; i++) { newptrstr[i] = 'A' + i; }
+	DEBUGLN(newptrstr);
+	free(newptrstr); DEBUGLN(newptrstr);
+	for (size_t i = 0; i < sizeof(xMainStack); i++) {
+		DEBUGF("%c", xMainStack[i]); DEBUG(' '); if ((i & 7) == 0) DEBUG('\n');
+		xMainStack[i] = 0;
+	}
+}
+
+void suicide_func2(void*) {
+	extern StackType_t* shitstack;
+	memset(xMainStack, 0xFF, 8192); dWrite(PIN_LED, LED_ON);
+	DEBUGLN("memset");/* free(shitstack);*/ vTaskDelete(NULL); return;
+	//Guru Meditation Error : Core  0 panic'ed (Instruction access fault). Exception was unhandled.
+	for (size_t i = 8192 / 4; i; --i) {
+		DEBUGF("%02X", ((uint32_t*)&shitstack)[i]); DEBUG(' '); if ((i & 7) == 0) DEBUG('\n');
+		((uint32_t*)&shitstack)[i] = 0;
+	}
+	DEBUGLN("LOL"); //***ERROR*** A stack overflow in task esp_timer has been detected.
+	vTaskDelete(NULL);
+}
+
+//static void timerAlarm(uint64_t value, stat_t flag, gptimer_handle_t& handle) {
+//	timer_restart(handle); timer_alarm(value, handle);
+//	Flag = flag;
+//}
