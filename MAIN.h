e10c1206a65489a30088b96f1682b47f3a71e009
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2025-06-09 10:19:21 +0300

    Update MAIN.h

--- MAIN.h
+++ MAIN.h
@@ -126,7 +126,7 @@ void main_init() {
 #endif
 }
 
-#ifndef  __cplusplus
+#ifdef  __cplusplus
 extern "C" {
 #endif //  __cplusplus
 	esp_err_t timer_alarm(uint64_t value, gptimer_handle_t& handle, bool reload = 1) {
@@ -156,33 +156,62 @@ extern "C" {
 	exit:log_v("ret = %u", ret);
 		return ret;
 	}
-#ifndef  __cplusplus
+#ifdef  __cplusplus
 }
 #endif //  __cplusplus
 
 void pinMode(uint8_t pin, uint8_t mode) {
-	if (mode >= 0x20) return; log_v("pinMode(%u, %u)", pin, mode);
-	gpio_hal_context_t gpiohal{ .dev = GPIO_LL_GET_HW(GPIO_PORT_0) };
+	if (mode >= 32) { log_d("pinMode(%u, %u)", pin, mode); return; }
+	//gpio_hal_context_t gpiohal{ .dev = GPIO_LL_GET_HW(GPIO_PORT_0) };
 	gpio_config_t conf = {
-	.pin_bit_mask = (1ULL << pin),              /*!< GPIO pin: set with bit mask, each bit maps to a GPIO */
-	.mode = GPIO_MODE_DISABLE,                  /*!< GPIO mode: set input/output mode                     */
-	.pull_up_en = GPIO_PULLUP_DISABLE,          /*!< GPIO pull-up                                         */
-	.pull_down_en = GPIO_PULLDOWN_DISABLE,      /*!< GPIO pull-down                                       */
-	.intr_type = (gpio_int_type_t)gpiohal.dev->pin[pin].int_type /*!< GPIO interrupt type - previously set                 */
-	};
-	if (mode < 0x20) {  //io
-		conf.mode = gpio_mode_t(mode & (INPUT | OUTPUT));
-		if (mode & OPEN_DRAIN) {
-			conf.mode = (gpio_mode_t)((int)conf.mode | GPIO_MODE_DEF_OD);
-		}
-		if (mode & PULLUP) {
-			conf.pull_up_en = GPIO_PULLUP_ENABLE;
-		}
-		if (mode & PULLDOWN) {
-			conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
-		}
+	.pin_bit_mask = (1ULL << pin),						/*!< GPIO pin: set with bit mask, each bit maps to a GPIO */
+		.mode = gpio_mode_t(mode & (INPUT | OUTPUT)),	/*!< GPIO mode: set input/output mode                     */
+		.pull_up_en = GPIO_PULLUP_DISABLE,				/*!< GPIO pull-up                                         */
+		.pull_down_en = GPIO_PULLDOWN_DISABLE,			/*!< GPIO pull-down                                       */
+		.intr_type = GPIO_INTR_DISABLE,
+		//(gpio_int_type_t)gpiohal.dev->pin[pin].int_type, /*!< GPIO interrupt type - previously set                 */
+	};  //io
+	if (mode & OPEN_DRAIN) {
+		conf.mode = (gpio_mode_t)((int)conf.mode | GPIO_MODE_DEF_OD);
+	}
+	if (mode & PULLUP) {
+		conf.pull_up_en = GPIO_PULLUP_ENABLE;
+	}
+	if (mode & PULLDOWN) {
+		conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
 	}
 	if (gpio_config(&conf) != ESP_OK) log_e("IO %i config failed", pin);
+}
+
+void digitalWrite(uint8_t pin, uint8_t val) {
+	gpio_set_level((gpio_num_t)pin, val);
+}
+
+int digitalRead(uint8_t pin) {
+	return gpio_get_level((gpio_num_t)pin);
+}
+
+void attachInterruptArg(uint8_t pin, voidFuncPtrArg userFunc, void* arg, int intr_type) {
+	if (pin >= SOC_GPIO_PIN_COUNT) return;// makes sure that pin -1 (255) will never work -- this follows Arduino standard
+	esp_err_t err = gpio_install_isr_service((int)ARDUINO_ISR_FLAG);
+	if (err != ESP_OK && err != ESP_ERR_INVALID_STATE) {
+		log_e("IO %i ISR Service Failed To Start", pin); return;
+	}
+	gpio_set_intr_type((gpio_num_t)pin, (gpio_int_type_t)(intr_type & 0b111));
+	if (intr_type & 0b1000) { gpio_wakeup_enable((gpio_num_t)pin, (gpio_int_type_t)(intr_type & 0b111)); }
+	gpio_isr_handler_add((gpio_num_t)pin, userFunc, arg);
+	gpio_hal_context_t gpiohal{ .dev = GPIO_LL_GET_HW(GPIO_PORT_0) }; //FIX interrupts on peripherals outputs (eg. LEDC,...) 
+	gpio_hal_input_enable(&gpiohal, pin); //Enable input in GPIO register
+}
+
+void attachInterrupt(uint8_t pin, voidFuncPtr handler, int mode) {
+	attachInterruptArg(pin, (voidFuncPtrArg)handler, NULL, mode);
+}
+
+void detachInterrupt(uint8_t pin) {
+	gpio_intr_disable((gpio_num_t)pin);
+	gpio_wakeup_disable((gpio_num_t)pin);
+	gpio_isr_handler_remove((gpio_num_t)pin);  //remove handle and disable isr for pin
 }
 //xTaskCreateUniversal(loopTask, "loopTask", getArduinoLoopTaskStackSize(), NULL, 1, &loopTaskHandle, ARDUINO_RUNNING_CORE);
 #undef CONFIG_AUTOSTART_ARDUINO
