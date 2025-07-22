644d6dc97eb078771ccadbb498f9f76df4b8e3ea
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2025-07-22 10:35:46 +0300

    Update credentials.h

--- credentials.h
+++ credentials.h
@@ -1,22 +1,28 @@
 #pragma once
 
+#define USER_ID ""
 #ifdef DEBUG_ENABLE
 #define BOT_TOKEN ""
-#define USER_ID ""
 #define CHAT_ID USER_ID
 #define SSID_HIDDEN 0
+#define STA_INIT_DELAY 1000
+#else
+#ifdef FIRST_BUILD
+#define BOT_TOKEN ""
+#define CHAT_ID ""
 #else
 #define BOT_TOKEN ""
-#define USER_ID ""
-#define CHAT_ID USER_ID// ""
+#define CHAT_ID USER_ID
+#endif
 #define SSID_HIDDEN 1
+#define STA_INIT_DELAY 100
 #endif // DEBUG_ENABLE
-#define ESP32C3_LUATOS
+
 #define DEFAULT_SSID ""
 #define DEFAULT_PASS ""
 #define AP_SSID ""
 #define AP_PASS ""
-#define WIFI_CHANNEL 13
+#define AP_WIFI_CHANNEL 13
 #define CHANGE_AUTH ""
 #define NVS_NAMESPACE "nvs.net80211"
 #define NVS_KEY_SSID "sta.ssid"
@@ -27,4 +33,4 @@
 #define RELAY_OFF "relay_off"
 #define SSID_PATH "/ssid.txt"
 #define PASS_PATH "/pass.txt"
-#define ALARM_PATH "/alarm.txt"
+#define ALARM_PATH "/alarm.bin"
