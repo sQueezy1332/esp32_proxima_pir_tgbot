f890c8894e27330cd8ad6ad37f7c147082601d21
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2025-11-21 14:42:10 +0300

    Update ESP_MAIN.h

--- ESP_MAIN.h
+++ ESP_MAIN.h
@@ -1,31 +1,18 @@
 #pragma once
-#ifndef H_ESP_MAIN_
-#define H_ESP_MAIN_
 //#define DEBUG_ENABLE
-//#define NO_GLOBAL_INSTANCES
-#define NO_GLOBAL_SERIAL
 #include <Arduino.h>
 //#include "esp_task_wdt.h"
 #include "nvs_flash.h"
 #include "esp_ota_ops.h"
-
+#include "hal/gpio_hal.h"
+#include "esp_check.h"
+/* #if defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG_ENABLED) && not defined ARDUINO_USB_CDC_ON_BOOT//CONFIG_USJ_ENABLE_USB_SERIAL_JTAG=y
+#define ARDUINO_USB_CDC_ON_BOOT 1
+#define ARDUINO_USB_MODE 1
+#endif */
 #if defined CONFIG_AUTOSTART_ARDUINO
 #pragma message "CONFIG_AUTOSTART_ARDUINO"
 #endif
-#if defined(CONFIG_BT_ENABLED) && SOC_BT_SUPPORTED
-#include "esp_bt.h" 
-#if CONFIG_IDF_TARGET_ESP32
-__weak_symbol bool  btInUse() __weak_symbol { return false; } //overwritten in esp32-hal-bt.c
-#else
-/*extern */__weak_symbol bool btInUse() { return true; }
-#endif
-#endif
-
-#if defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG_ENABLED) //CONFIG_USJ_ENABLE_USB_SERIAL_JTAG=y
-#define ARDUINO_USB_CDC_ON_BOOT 1
-#define ARDUINO_USB_MODE 1
-#endif
-
 #if (ARDUINO_USB_CDC_ON_BOOT | ARDUINO_USB_MSC_ON_BOOT | ARDUINO_USB_DFU_ON_BOOT) && !ARDUINO_USB_MODE
 #include "USB.h"
 #if ARDUINO_USB_MSC_ON_BOOT
@@ -33,56 +20,158 @@ __weak_symbol bool  btInUse() __weak_symbol { return false; } //overwritten in e
 #endif
 #endif
 
-#if ARDUHAL_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_DEBUG
-#include "chip-debug-report.h" 
-#define DEBUG_ENABLE
+#if defined(CONFIG_BLUEDROID_ENABLED) || defined(CONFIG_NIMBLE_ENABLED)
+#if CONFIG_IDF_TARGET_ESP32
+__weak_symbol bool btInUse() { return false; } //overwritten in esp32-hal-bt.c
+#else
+extern bool btInUse(); //from esp32-hal-bt.c
 #endif
+#endif
+
+/* #if ARDUHAL_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_DEBUG
+#define DEBUG_ENABLE
+#endif */
 #ifdef DEBUG_ENABLE
+#include "chip-debug-report.h" 
 #pragma message "DEBUG_ENABLE"
-#if ARDUINO_USB_CDC_ON_BOOT && ARDUINO_USB_MODE //Serial used from Native_USB_CDC | HW_CDC_JTAG        
-HWCDC HWCDCSerial; // Hardware CDC mode
-#define Serial HWCDCSerial // Arduino Serial is the HW JTAG CDC device
-#elif ARDUINO_USB_MODE// !ARDUINO_USB_MODE -- Native USB Mode
-USBCDC USBSerial(0); // Arduino Serial is the Native USB CDC device
-#define Serial USBSerial
-#else   // !ARDUINO_USB_CDC_ON_BOOT -- Serial is used from UART0
-extern HardwareSerial Serial0;
-#define Serial Serial0
-#endif  // ARDUINO_USB_CDC_ON_BOOT
-#define SerialBegin(x)  Serial.begin(x); vTaskDelay(1)
+#define SerialBegin(x)  Serial.begin(x); //while(!Serial){}
 #define DEBUG(x, ...) Serial.print(x, ##__VA_ARGS__)
 #define DEBUGLN(x, ...) Serial.println(x, ##__VA_ARGS__)
 #define DEBUGF(x, ...) Serial.printf(x , ##__VA_ARGS__)
 #define CHECK_(x) ESP_ERROR_CHECK_WITHOUT_ABORT(x)
-#define CHECK_RET(x) do {esp_err_t ret = (x);\
-        if (unlikely(ret != ESP_OK)) { log_e(" 0x%X\t(%s)", ret,  esp_err_to_name(ret)); return;} }while(0)//ESP_RETURN_VOID_ON_ERROR(%s)", err, 
+#define CHECK_RET(x) ESP_RETURN_VOID_ON_ERROR(x)
 #else
 #define DEBUG(x, ...)
 #define DEBUGLN(x, ...) 
 #define DEBUGF(x, ...)
 #define SerialBegin(x)
 #define CHECK_(x) (void)(x)
-#define CHECK_RET(x) do {esp_err_t ret = (x); if (unlikely(ret != ESP_OK)) {return;} }while(0)
+#define CHECK_RET(x) ESP_RETURN_VOID_ON_ERROR(x)//do {esp_err_t ret = (x); if (unlikely(ret != ESP_OK)) {return;} }while(0)
 #endif // DEBUG_ENABLE
 #define uS esp_timer_get_time()
-#define delayms(x) vTaskDelay((x) / portTICK_PERIOD_MS)
 #define delayUntil(prev, tmr) vTaskDelayUntil((prev),pdMS_TO_TICKS(tmr))
 #define SEC (1000000)
 #define ENTER_CRITICAL() {portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;portENTER_CRITICAL(&mux);
 #define EXIT_CRITICAL() portEXIT_CRITICAL(&mux);}
-
 typedef const char cch; typedef const uint8_t cbyte; typedef uint16_t u16; typedef uint32_t u32; typedef uint64_t u64;
 
 #ifdef CONFIG_APP_ROLLBACK_ENABLE
-esp_ota_img_states_t img_state(bool valid = false);
-__weak_symbol bool verifyRollbackLater();
-#endif 
+inline esp_ota_img_states_t img_state(bool valid = false) {
+	const esp_partition_t* running = esp_ota_get_running_partition();
+	esp_ota_img_states_t ota_state;
+	esp_ota_get_state_partition(running, &ota_state);
+	if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
+		log_d("IMG_PENDING_VERIFY %s" , valid ? "valid" : "");
+		if(valid) esp_ota_mark_app_valid_cancel_rollback();
+	}
+	return ota_state;
+}
+__weak_symbol bool verifyRollbackLater() { return true; };
+#endif
 
-void nvs_init();
-void main_init();
+inline void nvs_init() {
+	esp_err_t err = nvs_flash_init();
+	if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		const esp_partition_t* partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_NVS, NULL);
+		if (partition != NULL) {
+			err = esp_partition_erase_range(partition, 0, partition->size);
+			if (err != ESP_OK) { err = nvs_flash_init(); log_d("nvs_flash_init"); }
+			else log_e("Failed to format the broken NVS partition!");
+		}
+		else log_e("Could not find NVS partition");
+	}
+	if (err) log_e("Failed to initialize NVS! Error: %u", err);
+}
+
+inline void main_init() {
+	//init proper ref tick value for PLL (uncomment if REF_TICK is different than 1MHz)
+//ESP_REG(APB_CTRL_PLL_TICK_CONF_REG) = APB_CLK_FREQ / REF_CLK_FREQ - 1;
+#ifdef F_XTAL_MHZ
+#include "soc/rtc.h"
+#if !CONFIG_IDF_TARGET_ESP32S2  // ESP32-S2 does not support rtc_clk_xtal_freq_update
+	rtc_clk_xtal_freq_update((rtc_xtal_freq_t)F_XTAL_MHZ);
+	rtc_clk_cpu_freq_set_xtal();
+#endif
+#endif
+#ifdef F_CPU
+	//setCpuFrequencyMhz(F_CPU / 1000000);
+#endif
+	SerialBegin(115200); DEBUGLN("Serial begin");
+#if ARDUINO_USB_MSC_ON_BOOT && !ARDUINO_USB_MODE
+	MSC_Update.begin();
+#endif
+#if ARDUINO_USB_DFU_ON_BOOT && !ARDUINO_USB_MODE
+	USB.enableDFU();
+#endif
+#if ARDUINO_USB_ON_BOOT && !ARDUINO_USB_MODE
+	USB.begin();
+#endif
+#if (CONFIG_SPIRAM_SUPPORT || CONFIG_SPIRAM) && not defined CONFIG_SPIRAM_BOOT_INIT
+  psramAddToHeap();
+#endif
+	nvs_init();
+	//esp_log_level_set("*", CONFIG_LOG_DEFAULT_LEVEL);
+#if SOC_BT_SUPPORTED && __has_include("esp_bt.h")  && (defined(CONFIG_BLUEDROID_ENABLED) || defined(CONFIG_NIMBLE_ENABLED))
+	//if (!btInUse()) { //"esp_bt.h"
+	//	__unused esp_err_t ret = esp_bt_controller_mem_release(BT_MODE_BTDM); log_d("%i", ret);
+	//}
+#endif
+#ifdef CONFIG_BOOTLOADER_APP_ROLLBACK_ENABLE
+	if (!verifyRollbackLater()) { log_i("app_valid"); esp_ota_mark_app_valid_cancel_rollback(); }
+#endif
+}
+
+ void ARDUINO_ISR_ATTR pinMode(uint8_t pin, uint8_t mode) {
+	if (mode >= 32) { log_w("%u, %u", pin, mode); return; }
+	gpio_config_t conf = {
+		.pin_bit_mask = (1ULL << pin),						/*!< GPIO pin: set with bit mask, each bit maps to a GPIO */
+		.mode = (gpio_mode_t)(mode & GPIO_MODE_INPUT_OUTPUT_OD),	/*!<  GPIO mode: set input/output mode                     */
+		.pull_up_en = mode & PULLUP ? GPIO_PULLUP_ENABLE : GPIO_PULLUP_DISABLE,				/*!< GPIO pull-up                                         */
+		.pull_down_en = mode & PULLDOWN ? GPIO_PULLDOWN_ENABLE : GPIO_PULLDOWN_DISABLE,			/*!< GPIO pull-down                                       */
+		.intr_type = (gpio_int_type_t)GPIO_LL_GET_HW(GPIO_PORT_0)->pin[pin].int_type, /*!< GPIO interrupt type - previously set                 */
+	};
+	if (gpio_config(&conf) != ESP_OK) log_e("IO %i config failed", pin);
+}
+ 
+/*  __attribute__((__always_inline__)) inline */ void digitalWrite(uint8_t pin, uint8_t val) {
+    if (!digitalPinCanOutput(pin)) return;
+    gpio_hal_context_t gpiohal { .dev = GPIO_LL_GET_HW(GPIO_PORT_0) }; 
+	gpio_hal_set_level(&gpiohal, pin, val);
+} 
+
+int digitalRead(uint8_t pin) {
+	gpio_hal_context_t gpiohal { .dev = GPIO_LL_GET_HW(GPIO_PORT_0) }; //return gpio_ll_get_level(&GPIO, (gpio_num_t)pin);
+	return gpio_hal_get_level(&gpiohal, (gpio_num_t)pin);
+}
+
+void attachInterruptArg(uint8_t pin, void(*userFunc )(void*), void* arg, int intr_type) {
+	if (pin >= SOC_GPIO_PIN_COUNT) return;// makes sure that pin -1 (255) will never work -- this follows Arduino standard
+	esp_err_t err = gpio_install_isr_service((int)ARDUINO_ISR_FLAG);
+	if (err != ESP_OK) { log_e("IO %i ISR Service Failed To Start", pin); return; }
+	gpio_set_intr_type((gpio_num_t)pin, (gpio_int_type_t)(intr_type & 0b111));
+	if (intr_type & 0b1000) { gpio_wakeup_enable((gpio_num_t)pin, (gpio_int_type_t)(intr_type & 0b111)); }
+	gpio_isr_handler_add((gpio_num_t)pin, userFunc, arg);
+	gpio_hal_context_t gpiohal{ .dev = GPIO_LL_GET_HW(GPIO_PORT_0) }; //FIX interrupts on peripherals outputs (eg. LEDC,...) 
+	gpio_hal_input_enable(&gpiohal, pin); //Enable input in GPIO register
+}
+
+void attachInterrupt(uint8_t pin, void(*handler)() , int mode) {
+	attachInterruptArg(pin, (void(*)(void*))handler, NULL, mode);
+}
+
+void detachInterrupt(uint8_t pin) {
+	gpio_intr_disable((gpio_num_t)pin);
+	gpio_wakeup_disable((gpio_num_t)pin);
+	gpio_isr_handler_remove((gpio_num_t)pin);  //remove handle and disable isr for pin
+}
+
+void enableInterrupt(uint8_t pin) { gpio_intr_enable((gpio_num_t)pin); }
+
+void disableInterrupt(uint8_t pin) { gpio_intr_disable((gpio_num_t)pin); }
+
+//xTaskCreateUniversal(loopTask, "loopTask", getArduinoLoopTaskStackSize(), NULL, 1, &loopTaskHandle, ARDUINO_RUNNING_CORE); 
 class nvsApi {
-private:
-	nvs_handle_t _handle = 0;
+private: nvs_handle_t _handle = 0;
 public:
 	nvsApi() {};
 	nvsApi(const nvsApi &obj) = delete;
@@ -101,12 +190,66 @@ public:
 	operator nvs_handle_t() const { return _handle; };
 };
 
-/*void pinMode(uint8_t pin, uint8_t mode);
- 
- void digitalWrite(uint8_t pin, uint8_t val);
-int digitalRead(uint8_t pin);
-void attachInterruptArg(uint8_t pin, voidFuncPtrArg userFunc, void* arg, int intr_type);
-void attachInterrupt(uint8_t pin, voidFuncPtr handler, int mode);
-void detachInterrupt(uint8_t pin); */
-//xTaskCreateUniversal(loopTask, "loopTask", getArduinoLoopTaskStackSize(), NULL, 1, &loopTaskHandle, ARDUINO_RUNNING_CORE);
-#endif //H_ESP_MAIN_
+#include "esp_timer.h"
+
+inline esp_timer_handle_t 
+esp_timer_init( esp_timer_cb_t cb, esp_timer_dispatch_t type = ESP_TIMER_TASK, bool skip = false, void* arg = NULL, const char* name = NULL) {
+    esp_timer_handle_t handle; esp_err_t ret;
+    esp_timer_create_args_t config = {
+      .callback = cb,
+      .arg = arg,
+      .dispatch_method = type,
+      .name = name,
+      .skip_unhandled_events = skip,
+    };
+    ret = esp_timer_create(&config, &handle);
+    if (ret != ESP_OK) { ESP_ERROR_CHECK_WITHOUT_ABORT(ret); return NULL; };
+    return handle;
+}
+
+inline esp_err_t esp_timer_start(esp_timer_handle_t handle, uint64_t period) {
+  if (esp_timer_is_active(handle)) return esp_timer_restart(handle, period);
+  return esp_timer_start_once(handle, period); 
+}
+
+#include "driver/gptimer.h"
+
+#define gptimer_restart(x) gptimer_set_raw_count(x, 0)
+
+inline esp_err_t 
+timer_alarm(gptimer_handle_t handle, uint64_t value, bool reload = false, uint64_t count = 0) {
+	gptimer_alarm_config_t alarm_config{
+			.alarm_count = value,
+			.reload_count = count,
+			.flags = {.auto_reload_on_alarm = reload} //.flags.auto_reload_on_alarm = reload,
+	};
+	return gptimer_set_alarm_action(handle, &alarm_config);
+}
+
+inline gptimer_handle_t 
+timer_init(uint64_t value, gptimer_alarm_cb_t func, bool reload = false, uint8_t priority = 3) {
+		esp_err_t ret; gptimer_handle_t handle;
+		gptimer_config_t config {
+			.clk_src = GPTIMER_CLK_SRC_DEFAULT,
+			.direction = GPTIMER_COUNT_UP,
+			.resolution_hz = 1000000,
+			.intr_priority = priority,
+			.flags { .intr_shared = 1, .allow_pd = 0,.backup_before_sleep = 0 },
+		}; //SOC_TIMER_GROUP_TOTAL_TIMERS
+		gptimer_event_callbacks_t cbs = { .on_alarm = func };
+		if ((ret = gptimer_new_timer(&config, &handle)) 
+			|| (ret = gptimer_register_event_callbacks(handle, &cbs, NULL))
+			|| (ret = gptimer_enable(handle))
+			|| (ret = timer_alarm(handle, value, reload))) 
+			{ ESP_ERROR_CHECK_WITHOUT_ABORT(ret); return NULL;}
+		return handle;
+	}
+
+inline uint64_t timer_read(gptimer_handle_t handle) {
+	uint64_t value = 0;
+	esp_err_t ret = gptimer_get_raw_count(handle, &value);
+	ESP_ERROR_CHECK_WITHOUT_ABORT(ret);
+	return value;
+}
+
+
